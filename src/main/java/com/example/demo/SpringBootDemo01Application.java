c3bbd17e869d7141488b8ed94d83281f67a31680
Shreetharan <shreests07@gmail.com>
2025-07-08 16:31:09 +0530

    Updated SpringBootDemo01Application

--- src/main/java/com/example/demo/SpringBootDemo01Application.java
+++ src/main/java/com/example/demo/SpringBootDemo01Application.java
@@ -9,6 +9,7 @@ public class SpringBootDemo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemo01Application.class, args);
 		System.out.println("Hi Welcome to Spring Boot Applicattion ....!");
+		System.out.println("This is updated, it is for build 2");
 	}
 
 }
